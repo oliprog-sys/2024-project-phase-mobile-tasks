f6d3bf38ec70640cff6257b12591693981fd616d
Oliyad Diriba <oliyaddiriba75@gmail.com>
2025-12-02 21:20:05 +0300

    implment only the contract in the data repositories

--- on-boarding/data_overview_layer/lib/features/ecommerce/data/repositories/product_repository_impl.dart
+++ on-boarding/data_overview_layer/lib/features/ecommerce/data/repositories/product_repository_impl.dart
@@ -2,7 +2,6 @@
 import 'package:data_overview_layer/core/platform/network_info.dart';
 import 'package:data_overview_layer/features/ecommerce/data/data_source/product_local_data_source.dart';
 import 'package:data_overview_layer/features/ecommerce/data/data_source/product_remote_data_source.dart';
-import 'package:data_overview_layer/features/ecommerce/data/models/product_model.dart';
 import 'package:data_overview_layer/features/ecommerce/domain/entities/product.dart';
 import 'package:data_overview_layer/features/ecommerce/domain/repository/product_repository.dart';
 
@@ -21,57 +20,25 @@ class ProductRepositoryImpl extends ProductRepository {
   @override
   Future<void> deleteProduct(String id) async {
     // TODO: implement deleteProduct
-    if (await networkInfo.isConnected) {
-      await remoteDataSource.deleteProduct(id);
-    }
+   
   }
 
   @override
   Future<Product?> getProduct(String id) async {
+    return null;
+  
     // TODO: implement getProduct
-    if (await networkInfo.isConnected) {
-      final product = await remoteDataSource.fetchProduct(id);
-      if (product != null) {
-        await localDataSource.cacheProduct(product);
-      }
-      return product;
-    }
-    else {
-      return await localDataSource.getCachedProduct(id);
-    }
+    
   }
 
   @override
   Future<void> insertProduct(Product product) async {
-    // TODO: implement insertProduct
-    final productSample = ProductModel(
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      description: product.description,
-      imageUrl: product.imageUrl,
-    );
-    if (await networkInfo.isConnected) {
-      await remoteDataSource.insertProduct(productSample);
-    }
-    await localDataSource.cacheProduct(productSample);
+    // TODO: implement insertProduct    
   }
 
   @override
   Future<void> updateProduct(Product product) async {
-    // TODO: implement updateProduct
-    final productSample = ProductModel(
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      description: product.description,
-      imageUrl: product.imageUrl,
-    );
-
-    if (await networkInfo.isConnected) {
-      await remoteDataSource.updateProduct(productSample);
-    }
-    await localDataSource.cacheProduct(productSample);
+    // TODO: implement updateProduct    
   }
 
 }
